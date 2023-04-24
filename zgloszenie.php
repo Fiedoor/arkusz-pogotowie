88d3250235b4967637a082b4458273340c85237d
Stanisław Fiedoruk <11120@kasprzak.edu.pl>
2023-04-24 10:59:43 +0200

    Zrobione i o dziwo działa

---
+++ zgloszenie.php
@@ -0,0 +1,11 @@
+<?php
+error_reporting(0);
+$conn = mysqli_connect('localhost', 'root', '', 'ratownictwo');
+$numer_z = $_POST["numer_z"];
+$numer_d = $_POST["numer_d"];
+$adres = $_POST["adres"];
+$time = date('h') . ":" . date('i') . ":" . date('s');
+echo $time;
+mysqli_query($conn, "INSERT INTO `zgloszenia`(`ratownicy_id`, `dyspozytorzy_id`, `adres`, `pilne`, `czas_zgloszenia`) VALUES ('$numer_z','$numer_d','$adres','0','$time')");
+mysqli_close($conn);
+?>
