0ee9d6354a7a6c37523745a6d5ef1120a074c64e
Stanisław Fiedoruk <11120@kasprzak.edu.pl>
2023-04-25 08:15:29 +0200

    Dodany header i usunięte echo

--- zgloszenie.php
+++ zgloszenie.php
@@ -5,7 +5,7 @@ $numer_z = $_POST["numer_z"];
 $numer_d = $_POST["numer_d"];
 $adres = $_POST["adres"];
 $time = date('H:i:s');
-echo $time;
 mysqli_query($conn, "INSERT INTO `zgloszenia`(`ratownicy_id`, `dyspozytorzy_id`, `adres`, `pilne`, `czas_zgloszenia`) VALUES ('$numer_z','$numer_d','$adres','0','$time')");
 mysqli_close($conn);
+header("Location:pogotowie.html");
 ?>
